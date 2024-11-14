09566fa40c294391eb71a61011ccfbb60f51508f
iNanzo <newtonhoang99@gmail.com>
2024-11-14 14:38:49 -0800

    Updated comments

--- pyJson.py
+++ pyJson.py
@@ -1,7 +1,7 @@
-#Import Json class library
+# Import Json class library
 import json
 
-#Create dictionary array
+# Create dictionary array
 data1 = {
     "name":"Newton Hoang",
     "age":"24",
@@ -11,38 +11,37 @@ data1 = {
 }
 
 #####
-#with condition that converts data into a json file
+# Write the dictionary data into a JSON file with proper formatting
 with open("data1.json", "w") as json_file:
     json.dump(data1, json_file, indent = 4)
 
-#Confirmation prompt
+# Confirmation prompt
 print("Successfully written to data1.json")
 
 ##########
-#
-
+# Open the JSON file and load its content into a Python object (dictionary)
 with open("data1.json","r") as json_file:
-
     loaded_data = json.load(json_file)
 
-#Confirmation prompt
+# Confirmation prompt
 print("Successfully read data1.json")
-#Data output to terminal
+
+# Data output to terminal
 print(loaded_data)
 
 ##########
-#
-
+# Modify the loaded data1
 loaded_data["age"] = 18
 loaded_data["interests"].append("Board Games")
 loaded_data["interests"].remove("Art")
 
 ##########
-#
-
+# Write the modified data back into the JSON file, overwriting the old content
 with open("data1.json", "w") as json_file:
     json.dump(loaded_data, json_file, indent = 4)
 
-#Confirmation prompt
+# Confirmation prompt
 print("Successfully written to data1.json")
+
+# Output the updated data to the terminal
 print(loaded_data)
